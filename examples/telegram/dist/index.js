e1252c45fe8d72788f910d0ea034bec4ad346220
bluematter <michael@motionbox.io>
2024-01-31 23:37:53 -0800

    chore: update telegram bot example

--- examples/telegram/dist/index.js
+++ examples/telegram/dist/index.js
@@ -55,9 +55,11 @@ async function initializeBot() {
                     return;
                 }
                 const createResponse = await basedlabs.createImage({
-                    prompt: `${customPrompt}, cinematic, ultra detailed, 8K, <lora:ral-jeans-sdxl:1> ral-jeans`,
-                    modelId: 'ac8f3442feb931a2658a8c7c58dbadba8947dc58a5ccb81be26473de2f109aaa',
-                    negativePrompt: '(worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), morbid, ugly, asymmetrical, mutated malformed, mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (airbrushed, cartoon, anime, semi-realistic, cgi, render, blender, digital art, manga, amateur:1.3), (3D ,3D Game, 3D Game Scene, 3D Character:1.1), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3)',
+                    width: 512,
+                    height: 512,
+                    prompt: `a black ${customPrompt} made of thekittyandrea <lora:tktyndre:1>`,
+                    modelId: 'von-speed',
+                    negativePrompt: 'shirtless, shirtless woman, naked, porn, tits',
                 });
                 await bot.sendMessage(msg.chat.id, 'Processing your image, please wait, should take around 20s-30s...');
                 const pollForImage = async () => {
